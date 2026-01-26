3e99c9e01685fb92b3df0e131eada37d87f8f78e
chandumandapalli <chandumandapalli1@gmail.com>
2026-01-26 16:23:55 +0530

    Update ProtectedRoute.js

--- LLD/app/src/components/ProtectedRoute.js
+++ LLD/app/src/components/ProtectedRoute.js
@@ -1,9 +1,17 @@
 import { Outlet, Navigate } from "react-router-dom";
 
+// creating a seperate file in the components folder by name  as shown 
+// and do the need ful as shown below.. 
+
+
 const ProtectedRoute = () => {
   // Write Authentication Logic
   // Make login APi call, ceck if token valid
   const isAuthenticated = false;
+  //<Outlet /> is the place where children can render.. 
+  // so when the use is authencaited .. then all the child routes  are given  a permision to access them 
+  // and they can be rendered else . will redirectx to Login page .. as shown .. 
+  
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
