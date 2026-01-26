50d0ad84f0d35128b40429308027889e6fad110e
chandumandapalli <chandumandapalli1@gmail.com>
2026-01-26 15:15:43 +0530

    Update Shimmer.js

--- LLD/app/src/components/Shimmer.js
+++ LLD/app/src/components/Shimmer.js
@@ -1,4 +1,8 @@
 const Shimmer = () => {
+  // very imp thing to remember ..
+  // since Shimmer UI to be shown for certain elements of certain length .. 
+  // create Array ( length ) .. then fill(0) with some value s.. so that map can work on it .. so follow this rule ..
+  // Array( len ) .fill ( 0 )--- map elements .. 
   return Array(15)
     .fill(0)
     .map((n, i) => (
