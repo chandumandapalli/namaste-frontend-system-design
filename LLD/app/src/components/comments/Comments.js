3a50381a86a812ae37212e2c87c9c30393e5bdb7
chandumandapalli <chandumandapalli1@gmail.com>
2026-02-01 16:18:09 +0530

    Update Comments.js

--- LLD/app/src/components/comments/Comments.js
+++ LLD/app/src/components/comments/Comments.js
@@ -1,5 +1,9 @@
 import CommentBox from "./CommentBox";
 
+// always ask how do we get data .. fetch call . or lcoal data . or shal i design .. the data now .. 
+// this follows the concept of n -nested comments.. a shown below ( Reddit -- n nested comment structure ) 
+// U tube has only ( 2 nested comments ) 
+
 const data = [
   {
     username: "Akshay Saini",
